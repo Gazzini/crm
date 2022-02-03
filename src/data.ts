27b29d794bc41b7e5a5918a20be623fb5766018c
gazzini <gazzini.john@gmail.com>
2022-02-02 23:30:09 -0600

    test

--- src/data.ts
+++ src/data.ts
@@ -9,24 +9,24 @@ const pbkdf2 = util.promisify(crypto.pbkdf2);
 
 const USER_UUID_NAMESPACE = "9738E54D-3350-402B-9849-35F0ECEB772C";
 
-export type MessageId = string;
+// export type MessageId = string;
 
-export interface Message {
-  value: {
-    id: MessageId;
-    from: UserId;
-    convId: ConversationId;
-    text: string;
-  };
-}
+// export interface Message {
+//   value: {
+//     id: MessageId;
+//     from: UserId;
+//     convId: ConversationId;
+//     text: string;
+//   };
+// }
 
-export type ConversationId = string;
+// export type ConversationId = string;
 
-export interface Conversation {
-  value: {
-    convId: ConversationId;
-  };
-}
+// export interface Conversation {
+//   value: {
+//     convId: ConversationId;
+//   };
+// }
 
 export type UserId = string;
 
@@ -39,98 +39,98 @@ export interface User {
   salt: string;
 }
 
-export interface ItemList<T> {
-  items: T[];
-}
+// export interface ItemList<T> {
+//   items: T[];
+// }
 
-export async function getConversations(userId: UserId) {
-  const { items } = (await data.get(`user_${userId}:conv_*`)) as any;
-  for (const item of items) {
-    item.value.userIds = JSON.parse(item.value.userIds);
-  }
-  items.sort((a, b) =>
-    b.value.mtime > a.value.mtime ? 1 : a.value.mtime > b.value.mtime ? -1 : 0
-  );
-  return { items };
-}
+// export async function getConversations(userId: UserId) {
+//   const { items } = (await data.get(`user_${userId}:conv_*`)) as any;
+//   for (const item of items) {
+//     item.value.userIds = JSON.parse(item.value.userIds);
+//   }
+//   items.sort((a, b) =>
+//     b.value.mtime > a.value.mtime ? 1 : a.value.mtime > b.value.mtime ? -1 : 0
+//   );
+//   return { items };
+// }
 
-export async function getMessages(convId: string): Promise<ItemList<Message>> {
-  return data.get(`conv_${convId}:msg_*`);
-}
+// // export async function getMessages(convId: string): Promise<ItemList<Message>> {
+// //   return data.get(`conv_${convId}:msg_*`);
+// // }
 
-async function createUserConversation(
-  convId: ConversationId,
-  user: User,
-  users: Array<User>
-) {
-  const userId = user.id;
-  const otherUsers = users.filter((u) => u.id !== userId);
+// async function createUserConversation(
+//   convId: ConversationId,
+//   user: User,
+//   users: Array<User>
+// ) {
+//   const userId = user.id;
+//   const otherUsers = users.filter((u) => u.id !== userId);
 
-  return data.set(
-    `user_${user.id}:conv_${convId}`,
-    {
-      convId,
-      userId,
-      userIds: JSON.stringify(otherUsers.map((u) => u.id)),
-      title: otherUsers.map((u) => u.name).join(","),
-      picture: otherUsers[0].picture,
-    },
-    { label1: `conv_${convId}:user_${userId}` }
-  );
-}
+//   return data.set(
+//     `user_${user.id}:conv_${convId}`,
+//     {
+//       convId,
+//       userId,
+//       userIds: JSON.stringify(otherUsers.map((u) => u.id)),
+//       title: otherUsers.map((u) => u.name).join(","),
+//       picture: otherUsers[0].picture,
+//     },
+//     { label1: `conv_${convId}:user_${userId}` }
+//   );
+// }
 
-export async function createConversation(
-  userIds: UserId[]
-): Promise<Conversation> {
-  const convId = (await ksuid.random()).string;
-  const users = await Promise.all(
-    userIds.map((userId) => data.get(`user:${userId}`))
-  );
+// export async function createConversation(
+//   userIds: UserId[]
+// ): Promise<Conversation> {
+//   const convId = (await ksuid.random()).string;
+//   const users = await Promise.all(
+//     userIds.map((userId) => data.get(`user:${userId}`))
+//   );
 
-  // Create a conversation item for each user
-  await Promise.all(
-    users.map((user: any) => createUserConversation(convId, user, users))
-  );
+//   // Create a conversation item for each user
+//   await Promise.all(
+//     users.map((user: any) => createUserConversation(convId, user, users as any))
+//   );
 
-  return { value: { convId } };
-}
+//   return { value: { convId } };
+// }
 
-export async function sendMessage(
-  convId: string,
-  fromUserId: UserId,
-  text: string
-): Promise<Message> {
-  const messageId = (await ksuid.random()).string;
+// export async function sendMessage(
+//   convId: string,
+//   fromUserId: UserId,
+//   text: string
+// ): Promise<Message> {
+//   const messageId = (await ksuid.random()).string;
 
-  const message = {
-    id: messageId,
-    from: fromUserId,
-    convId,
-    text,
-  };
+//   const message = {
+//     id: messageId,
+//     from: fromUserId,
+//     convId,
+//     text,
+//   };
 
-  await data.set(`conv_${convId}:msg_${message.id}`, message);
+//   await data.set(`conv_${convId}:msg_${message.id}`, message);
 
-  return { value: message };
-}
+//   return { value: message };
+// }
 
-data.on("created:conv_*:msg_*", async ({ item }) => {
-  const { convId, text } = item.value;
+// data.on("created:conv_*:msg_*", async ({ item }) => {
+//   const { convId, text } = item.value;
 
-  const { items: userConversations } = await data.getByLabel(
-    "label1",
-    `conv_${convId}:user_*`
-  );
+//   const { items: userConversations } = await data.getByLabel(
+//     "label1",
+//     `conv_${convId}:user_*`
+//   );
 
-  await Promise.all(
-    userConversations.map(async ({ value }) => {
-      await data.set(`user_${value.userId}:conv_${convId}`, {
-        last: text,
-        mtime: item.modified,
-      });
-    })
-  );
-});
+//   await Promise.all(
+//     userConversations.map(async ({ value }) => {
+//       await data.set(`user_${value.userId}:conv_${convId}`, {
+//         last: text,
+//         mtime: item.modified,
+//       });
+//     })
+//   );
+// });
 
 export async function createUser({ username, name, password }): Promise<User> {
   const userId = uuidv5(username, USER_UUID_NAMESPACE);
@@ -152,9 +152,9 @@ export async function createUser({ username, name, password }): Promise<User> {
   return result;
 }
 
-export async function getUser(userId: UserId): Promise<User> {
-  return (await data.get(`user:${userId}`)) as unknown as User;
-}
+// export async function getUser(userId: UserId): Promise<User> {
+//   return (await data.get(`user:${userId}`)) as unknown as User;
+// }
 
 export async function getUserForSub(sub: string): Promise<User> {
   const userId = uuidv5(sub, USER_UUID_NAMESPACE);
@@ -162,92 +162,92 @@ export async function getUserForSub(sub: string): Promise<User> {
   return (await data.get(`user:${userId}`)) as unknown as User;
 }
 
-export async function updateUser(userId: User, props: any) {
-  const geohash = props.lat && geo.hash(props.lat, props.lon);
-  const user = await data.set(
-    `user:${userId}`,
-    {
-      ...props,
-      geohash,
-    },
-    geohash && { label1: `users:geo_${geohash}` }
-  );
-  return user;
-}
+// export async function updateUser(userId: User, props: any) {
+//   const geohash = props.lat && geo.hash(props.lat, props.lon);
+//   const user = await data.set(
+//     `user:${userId}`,
+//     {
+//       ...props,
+//       geohash,
+//     },
+//     geohash && { label1: `users:geo_${geohash}` }
+//   );
+//   return user;
+// }
 
-export async function listUsersInRect(rect: Rect): Promise<ItemList<User>> {
-  const region = geo.region(rect);
-  const cells = geo.coverRegion(region);
+// export async function listUsersInRect(rect: Rect): Promise<ItemList<User>> {
+//   const region = geo.region(rect);
+//   const cells = geo.coverRegion(region);
 
-  const results = await Promise.all(
-    cells.map((cell) => {
-      return data.getByLabel(
-        "label1",
-        `users:` +
-          `geo_${cell.rangeMin().toToken()}|` +
-          `geo_${cell.rangeMax().toToken()}`
-      );
-    })
-  );
+//   const results = await Promise.all(
+//     cells.map((cell) => {
+//       return data.getByLabel(
+//         "label1",
+//         `users:` +
+//           `geo_${cell.rangeMin().toToken()}|` +
+//           `geo_${cell.rangeMax().toToken()}`
+//       );
+//     })
+//   );
 
-  const items = [];
-  for (var result of results) {
-    items.push(
-      ...result.items.filter(({ value }) => {
-        return region.containsLL(geo.point(value.lat, value.lon));
-      })
-    );
-  }
+//   const items = [];
+//   for (var result of results) {
+//     items.push(
+//       ...result.items.filter(({ value }) => {
+//         return region.containsLL(geo.point(value.lat, value.lon));
+//       })
+//     );
+//   }
 
-  return { items };
-}
+//   return { items };
+// }
 
-export async function listUsersInCircle(
-  center: GeoPoint,
-  radius: number
-): Promise<ItemList<User>> {
-  const cells = geo.coverCircle(center, radius);
+// export async function listUsersInCircle(
+//   center: GeoPoint,
+//   radius: number
+// ): Promise<ItemList<User>> {
+//   const cells = geo.coverCircle(center, radius);
 
-  const results = await Promise.all(
-    cells.map((cell) => {
-      return data.getByLabel(
-        "label1",
-        `users:` +
-          `geo_${cell.rangeMin().toToken()}|` +
-          `geo_${cell.rangeMax().toToken()}`
-      );
-    })
-  );
+//   const results = await Promise.all(
+//     cells.map((cell) => {
+//       return data.getByLabel(
+//         "label1",
+//         `users:` +
+//           `geo_${cell.rangeMin().toToken()}|` +
+//           `geo_${cell.rangeMax().toToken()}`
+//       );
+//     })
+//   );
 
-  const items = [];
-  for (var result of results) {
-    items.push(
-      ...result.items.filter(({ value }) =>
-        geo.pointInCircle({ lat: value.lat, lon: value.lon }, center, radius)
-      )
-    );
-  }
-  return { items };
-}
+//   const items = [];
+//   for (var result of results) {
+//     items.push(
+//       ...result.items.filter(({ value }) =>
+//         geo.pointInCircle({ lat: value.lat, lon: value.lon }, center, radius)
+//       )
+//     );
+//   }
+//   return { items };
+// }
 
-export async function listAllUsers(): Promise<ItemList<User>> {
-  const result = await data.get("user:*");
-  return result;
-}
+// export async function listAllUsers(): Promise<ItemList<User>> {
+//   const result = await data.get("user:*");
+//   return result;
+// }
 
-export async function setTyping(
-  userId: UserId,
-  convId: ConversationId,
-  typing: boolean
-): Promise<void> {
-  const { items } = await data.getByLabel("label1", `conv_${convId}:user_*`);
-  await Promise.all(
-    items
-      .filter(({ value }) => value.userId !== userId)
-      .map(({ value }) =>
-        data.set(`user_${value.userId}:conv_${value.convId}`, {
-          typing,
-        })
-      )
-  );
-}
+// export async function setTyping(
+//   userId: UserId,
+//   convId: ConversationId,
+//   typing: boolean
+// ): Promise<void> {
+//   const { items } = await data.getByLabel("label1", `conv_${convId}:user_*`);
+//   await Promise.all(
+//     items
+//       .filter(({ value }) => value.userId !== userId)
+//       .map(({ value }) =>
+//         data.set(`user_${value.userId}:conv_${value.convId}`, {
+//           typing,
+//         })
+//       )
+//   );
+// }
--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import schema from './schema';
 import { resolvers } from './resolvers';
 
@@ -6,14 +7,23 @@ const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
     context: ({ req, res }) => {
+        // TODO: auth here?
         return { req, res };
     },
-    playground: {
-        settings: {
-            'editor.theme': 'dark',
-            'request.credentials': 'include'
-        }
-    },
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground({
+            settings: {
+              'editor.theme': 'dark',
+              'editor.cursorShape': 'line',
+              'editor.reuseHeaders': true,
+              'tracing.hideTracingResponse': true,
+              'queryPlan.hideQueryPlanResponse': true,
+              'editor.fontSize': 16,
+              'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
+              'request.credentials': 'include',
+            },
+        }),
+    ],
     introspection: true,
     debug: true
 });
