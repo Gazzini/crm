12bb2e8be38dc7b1746099208cc706ad2ad152ab
gazzini <gazzini.john@gmail.com>
2022-02-04 12:07:05 -0600

    case fix part 2

---
+++ src/graphql/Resolvers.ts
@@ -0,0 +1,17 @@
+import { Accounts } from '../handler/Accounts';
+import { Resolvers } from './generated/resolvers-types';
+import { GQLContext } from './GQLContext';
+
+/// These map queries & mutations to handler functions.
+export const resolvers: Resolvers<GQLContext> = {
+    Query: {
+        version: () => '1.0',
+        hello: () => 'World!',
+        me: (_, __, ctx) => Accounts.getUserId({ ctx }),
+    },
+    Mutation: {
+        goodbye: () => 'Cruel world!',
+        register: async (_, { username, email, password }, ctx) => await Accounts.registerUser({ username, email, password, ctx, }),
+        login: async (_, { email, password }, ctx) => await Accounts.loginUser({ email, password, ctx, }),
+    },
+};
---
+++ src/graphql/Server.ts
@@ -0,0 +1,48 @@
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import schema from './Schema';
+import { resolvers } from './Resolvers';
+import { Express } from "express";
+import { GQLContext } from './GQLContext';
+
+
+/// This is slightly harder than it should be because ApolloServer loads async, and
+/// we have to initialize it before calling applyMiddleware(...)
+export const applyGraphqlToExpressApp = async (app: Express) => {
+    
+    const server = new ApolloServer<GQLContext>({
+        typeDefs: schema,
+        resolvers,
+        // TODO: more explicit typing here?
+        context: ({ req, res }) => {
+            return { req, res };
+        },
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground({
+                settings: {
+                  'editor.theme': 'dark',
+                  'editor.cursorShape': 'line',
+                  'editor.reuseHeaders': true,
+                  'tracing.hideTracingResponse': true,
+                  'queryPlan.hideQueryPlanResponse': true,
+                  'editor.fontSize': 16,
+                  'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
+                  'request.credentials': 'include',
+                },
+            }),
+        ],
+        introspection: true,
+        debug: true
+    });
+
+    console.log('gql is starting...');
+    await server.start();
+
+    server.applyMiddleware({
+        app,
+        path: '/graphql',
+        cors: false,
+    });
+
+    console.log('gql is ready!');
+}
